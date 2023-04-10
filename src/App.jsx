91489ab65ccd6a6e41950ca7f4cb9287750b39db
Samantha Iraheta <sairaheta1@gmail.com>
2023-04-09 19:06:39 -0700

    started new vite project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./styles.css"
+
+export default function App() {
+  return (
+    <>
+      <form className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item" id="item">New Item</label>
+          <input type="text" id="item"/>
+        </div>
+        <button className="btn">Add</button>
+      </form>
+  <h1 className="header">Todo List</h1>
+    </>
+  
+  )
+}
