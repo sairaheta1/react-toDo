fce34d7dad92a17c9dfd08e4227d1dcb735c95f7
Samantha Iraheta <sairaheta1@gmail.com>
2023-04-09 19:22:39 -0700

    finished basic template of to-do list and added useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import "./styles.css"
+import { useState } from "react"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  //setNewItem wil re-render ALL of JS
+
   return (
     <>
       <form className="new-item-form">
         <div className="form-row">
           <label htmlFor="item" id="item">New Item</label>
-          <input type="text" id="item"/>
+          <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
         </div>
         <button className="btn">Add</button>
       </form>
-  <h1 className="header">Todo List</h1>
+      <h1 className="header">Todo List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox"/>
+            Item 1
+          </label>
+          <button className="btn btn-delete">Delete</button>
+        </li>
+      </ul>
     </>
   
   )
