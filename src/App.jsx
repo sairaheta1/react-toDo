868a1708aa0c7a10bd1a3f0ce49de712130c8d66
Samantha Iraheta <sairaheta1@gmail.com>
2023-04-09 19:38:20 -0700

    added functioning to update array once a new item is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import "./styles.css"
 import { useState } from "react"
 
 export default function App() {
-  const [newItem, setNewItem] = useState("")
-  //setNewItem wil re-render ALL of JS
+  const [newItem, setNewItem] = useState("") //setNewItem wil re-render ALL of JS
+
+  const [todos, setTodos] = useState([]) //want an empty array for our TODOs
+
+  function handleSubmit(e) {
+    e.preventDefault() //prevents refreshing of page when adding new item
+
+    setTodos((currentTodos) => { //update our array of todo items
+      return [
+        ...currentTodos, {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+  }
 
   return (
     <>
-      <form className="new-item-form">
+      <form onSubmit={handleSubmit} className="new-item-form">
         <div className="form-row">
           <label htmlFor="item" id="item">New Item</label>
           <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item"/>
