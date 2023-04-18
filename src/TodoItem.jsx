e705b839c3b0deae2a2ab922c285a488f767698c
Samantha Iraheta <sairaheta1@gmail.com>
2023-04-17 17:24:27 -0700

    finsihed stlying for content within list

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,11 +1,11 @@
 export function TodoItem({ completed, id, title, toggleTodo, deleteTodo }) {
   return (
-    <li>
-      <label>
+    <li className="item">
+      <label className="item-label">
         <input type="checkbox" checked={completed} onChange={e => toggleTodo(id, e.target.checked)}/>
         {title}
       </label>
-      <button className="btn btn-delete" onClick={() => deleteTodo(id)}>Delete</button>
+      <button className="btn btn-delete" onClick={() => deleteTodo(id)}>X</button>
     </li>
   )
 }
